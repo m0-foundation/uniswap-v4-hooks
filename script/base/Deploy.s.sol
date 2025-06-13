4a223d96073de95c0e4f0f7fa9fbf9ce3ccfb369
Pierrick Turelier <pierrick@turelier.com>
2025-06-13 12:47:38 +0200

    fix(Deploy): correct tick bounds order (#8)
    * fix(Deploy): correct tick bounds order
    
    * fix(Deploy): use sqrt price at tick 0.5 (#10)
    
    * test(Deploy): fix deploy test

--- script/base/Deploy.s.sol
+++ script/base/Deploy.s.sol
@@ -105,17 +105,15 @@ contract Deploy is Config, Script {
     }
 
     function _deployPool(DeployConfig memory config_, IHooks hook_) internal returns (PoolKey memory pool_) {
-        (Currency currency0_, Currency currency1_) = _sortCurrencies(address(config_.usdc), address(config_.wrappedM));
-
         pool_ = PoolKey({
-            currency0: currency0_,
-            currency1: currency1_,
+            currency0: config_.currency0,
+            currency1: config_.currency1,
             fee: config_.fee,
             tickSpacing: config_.tickSpacing,
             hooks: hook_
         });
 
-        IPoolManager(config_.poolManager).initialize(pool_, TickMath.getSqrtPriceAtTick(0));
+        IPoolManager(config_.poolManager).initialize(pool_, 79230143144055126352967237632); // Sqrt Price at tick 0.5
     }
 
     function _logPoolDeployment(Vm.Log[] memory logs_) internal pure {
@@ -133,14 +131,5 @@ contract Deploy is Config, Script {
                 console.log("Pool hooks:", address(eventHooks_));
             }
         }
-    }
-
-    function _sortCurrencies(
-        address tokenA_,
-        address tokenB_
-    ) internal pure returns (Currency currency0_, Currency currency1_) {
-        (currency0_, currency1_) = tokenA_ < tokenB_
-            ? (Currency.wrap(tokenA_), Currency.wrap(tokenB_))
-            : (Currency.wrap(tokenB_), Currency.wrap(tokenA_));
     }
 }
--- test/deploy/Deploy.t.sol
+++ test/deploy/Deploy.t.sol
@@ -9,6 +9,7 @@ import { IHooks } from "../../lib/v4-periphery/lib/v4-core/src/interfaces/IHooks
 import { IPoolManager } from "../../lib/v4-periphery/lib/v4-core/src/interfaces/IPoolManager.sol";
 
 import { StateLibrary } from "../../lib/v4-periphery/lib/v4-core/src/libraries/StateLibrary.sol";
+import { TickMath } from "../../lib/v4-periphery/lib/v4-core/src/libraries/TickMath.sol";
 
 import { Currency } from "../../lib/v4-periphery/lib/v4-core/src/types/Currency.sol";
 import { PoolKey } from "../../lib/v4-periphery/lib/v4-core/src/types/PoolKey.sol";
@@ -28,9 +29,11 @@ contract DeployTest is Deploy, Test {
     address public constant MANAGER = 0x431169728D75bd02f4053435b87D15c8d1FB2C72; // M0 Labs
 
     uint256 public mainnetFork;
+    uint256 public optimismFork;
 
     function setUp() public {
         mainnetFork = vm.createFork(vm.envString("MAINNET_RPC_URL"));
+        optimismFork = vm.createFork(vm.envString("OPTIMISM_RPC_URL"));
     }
 
     /* ============ getConfig ============ */
@@ -57,8 +60,13 @@ contract DeployTest is Deploy, Test {
         assertTrue(IAllowlistHook(allowlistHook_).isSwapRouterTrusted(config.swapRouter));
         assertTrue(IAllowlistHook(allowlistHook_).isPositionManagerTrusted(address(config.posm)));
 
-        assertEq(IBaseTickRangeHook(allowlistHook_).tickLowerBound(), config.tickLowerBound);
-        assertEq(IBaseTickRangeHook(allowlistHook_).tickUpperBound(), config.tickUpperBound);
+        int24 tickLowerBound_ = IBaseTickRangeHook(allowlistHook_).tickLowerBound();
+        int24 tickUpperBound_ = IBaseTickRangeHook(allowlistHook_).tickUpperBound();
+
+        assertEq(tickLowerBound_, 0);
+        assertEq(tickUpperBound_, 1);
+        assertEq(getPriceAtTick(tickLowerBound_), 1e18);
+        assertEq(getPriceAtTick(tickUpperBound_), 1000100000000000000);
     }
 
     function testFork_deployAllowlistHookAndPool() public {
@@ -71,8 +79,57 @@ contract DeployTest is Deploy, Test {
 
         PoolKey memory poolKey_ = _deployPool(config, IHooks(allowlistHook_));
 
-        assertEq(Currency.unwrap(poolKey_.currency0), address(config.wrappedM));
-        assertEq(Currency.unwrap(poolKey_.currency1), address(config.usdc));
+        assertEq(Currency.unwrap(poolKey_.currency0), WRAPPED_M);
+        assertEq(Currency.unwrap(poolKey_.currency1), USDC_ETHEREUM);
+        assertEq(poolKey_.fee, config.fee);
+        assertEq(poolKey_.tickSpacing, config.tickSpacing);
+        assertEq(address(poolKey_.hooks), address(allowlistHook_));
+
+        (, int24 tickCurrent_, , ) = IBaseHook(allowlistHook_).poolManager().getSlot0(poolKey_.toId());
+        assertEq(tickCurrent_, 0);
+    }
+
+    function testFork_deployAllowlistHook_optimism() public {
+        // TODO: skipping for now since Predicate's service manager is not deployed on optimism yet
+        //       and deployment fails when initializing it.
+        vm.skip(true);
+        vm.selectFork(optimismFork);
+
+        DeployConfig memory config = _getDeployConfig(block.chainid);
+
+        vm.prank(CREATE2_DEPLOYER);
+        address allowlistHook_ = _deployAllowlistHook(ADMIN, MANAGER, config);
+
+        assertTrue(IAccessControl(allowlistHook_).hasRole(bytes32(0x00), ADMIN));
+        assertTrue(IAccessControl(allowlistHook_).hasRole(keccak256("MANAGER_ROLE"), MANAGER));
+
+        assertTrue(IAllowlistHook(allowlistHook_).isSwapRouterTrusted(config.swapRouter));
+        assertTrue(IAllowlistHook(allowlistHook_).isPositionManagerTrusted(address(config.posm)));
+
+        int24 tickLowerBound_ = IBaseTickRangeHook(allowlistHook_).tickLowerBound();
+        int24 tickUpperBound_ = IBaseTickRangeHook(allowlistHook_).tickUpperBound();
+
+        assertEq(tickLowerBound_, -1);
+        assertEq(tickUpperBound_, 0);
+        assertEq(getPriceAtTick(tickLowerBound_), 999900009999000099);
+        assertEq(getPriceAtTick(tickUpperBound_), 1e18);
+    }
+
+    function testFork_deployAllowlistHookAndPool_optimism() public {
+        // TODO: skipping for now since Predicate's service manager is not deployed on optimism yet
+        //       and deployment fails when initializing it.
+        vm.skip(true);
+        vm.selectFork(optimismFork);
+
+        DeployConfig memory config = _getDeployConfig(block.chainid);
+
+        vm.prank(CREATE2_DEPLOYER);
+        address allowlistHook_ = _deployAllowlistHook(ADMIN, MANAGER, config);
+
+        PoolKey memory poolKey_ = _deployPool(config, IHooks(allowlistHook_));
+
+        assertEq(Currency.unwrap(poolKey_.currency0), WRAPPED_M);
+        assertEq(Currency.unwrap(poolKey_.currency1), USDC_ETHEREUM);
         assertEq(poolKey_.fee, config.fee);
         assertEq(poolKey_.tickSpacing, config.tickSpacing);
         assertEq(address(poolKey_.hooks), address(allowlistHook_));
@@ -108,13 +165,63 @@ contract DeployTest is Deploy, Test {
 
         PoolKey memory poolKey_ = _deployPool(config, IHooks(tickRangeHook_));
 
-        assertEq(Currency.unwrap(poolKey_.currency0), address(config.wrappedM));
-        assertEq(Currency.unwrap(poolKey_.currency1), address(config.usdc));
+        assertEq(Currency.unwrap(poolKey_.currency0), WRAPPED_M);
+        assertEq(Currency.unwrap(poolKey_.currency1), USDC_ETHEREUM);
         assertEq(poolKey_.fee, config.fee);
         assertEq(poolKey_.tickSpacing, config.tickSpacing);
         assertEq(address(poolKey_.hooks), tickRangeHook_);
 
         (, int24 tickCurrent_, , ) = IBaseHook(tickRangeHook_).poolManager().getSlot0(poolKey_.toId());
         assertEq(tickCurrent_, 0);
+    }
+
+    function testFork_deployTickRangeHook_optimism() public {
+        vm.selectFork(optimismFork);
+
+        DeployConfig memory config = _getDeployConfig(block.chainid);
+
+        vm.prank(CREATE2_DEPLOYER);
+        address tickRangeHook_ = _deployTickRangeHook(ADMIN, MANAGER, config);
+
+        assertTrue(IAccessControl(tickRangeHook_).hasRole(bytes32(0x00), ADMIN));
+        assertTrue(IAccessControl(tickRangeHook_).hasRole(keccak256("MANAGER_ROLE"), MANAGER));
+
+        int24 tickLowerBound_ = IBaseTickRangeHook(tickRangeHook_).tickLowerBound();
+        int24 tickUpperBound_ = IBaseTickRangeHook(tickRangeHook_).tickUpperBound();
+
+        assertEq(tickLowerBound_, -1);
+        assertEq(tickUpperBound_, 0);
+        assertEq(getPriceAtTick(tickLowerBound_), 999900009999000099);
+        assertEq(getPriceAtTick(tickUpperBound_), 1e18);
+    }
+
+    function testFork_deployTickRangeHookAndPool_optimism() public {
+        vm.selectFork(optimismFork);
+
+        DeployConfig memory config = _getDeployConfig(block.chainid);
+
+        vm.prank(CREATE2_DEPLOYER);
+        address tickRangeHook_ = _deployTickRangeHook(ADMIN, MANAGER, config);
+
+        PoolKey memory poolKey_ = _deployPool(config, IHooks(tickRangeHook_));
+
+        assertEq(Currency.unwrap(poolKey_.currency0), USDC_OPTIMISM);
+        assertEq(Currency.unwrap(poolKey_.currency1), WRAPPED_M);
+        assertEq(poolKey_.fee, config.fee);
+        assertEq(poolKey_.tickSpacing, config.tickSpacing);
+        assertEq(address(poolKey_.hooks), tickRangeHook_);
+
+        (, int24 tickCurrent_, , ) = IBaseHook(tickRangeHook_).poolManager().getSlot0(poolKey_.toId());
+        assertEq(tickCurrent_, 0);
+    }
+
+    /* ============ Utils ============ */
+
+    function getPriceAtTick(int24 tick) public pure returns (uint256) {
+        uint160 sqrtPriceX96 = TickMath.getSqrtPriceAtTick(tick);
+        uint256 priceX192 = uint256(sqrtPriceX96) * uint256(sqrtPriceX96);
+        require(priceX192 > 0, "Invalid sqrtPrice");
+
+        return (priceX192 * 1e18) / (1 << 192);
     }
 }
